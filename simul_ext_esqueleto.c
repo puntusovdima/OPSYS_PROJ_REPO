af8b36753fb5a33ec8c64efd25c5cb9dd88792c4
sam10003 <sam.porta2005@gmail.com>
2024-12-16 19:57:18 +0100

    sorry carlos

--- simul_ext_esqueleto.c
+++ simul_ext_esqueleto.c
@@ -1,12 +1,18 @@
-#include<stdio.h>
-#include<string.h>
-#include<ctype.h>
+
+#include <stdio.h>
+#include <string.h>
+#include <ctype.h>
+#include <stdlib.h>
+
 #include "headers.h"
 
 #define LONGITUD_COMANDO 100
 
+//let us begin with the first and most essential function
+//int ComprobarCommando(char *strcomando, char *orden, char *argumento1, char *argumento2);
+
+/*
 void Printbytemaps(EXT_BYTE_MAPS *ext_bytemaps);
-int ComprobarComando(char *strcomando, char *orden, char *argumento1, char *argumento2);
 void LeeSuperBloque(EXT_SIMPLE_SUPERBLOCK *psup);
 int BuscaFich(EXT_ENTRADA_DIR *directorio, EXT_BLQ_INODOS *inodos, 
               char *nombre);
@@ -25,68 +31,118 @@ void Grabarinodosydirectorio(EXT_ENTRADA_DIR *directorio, EXT_BLQ_INODOS *inodos
 void GrabarByteMaps(EXT_BYTE_MAPS *ext_bytemaps, FILE *fich);
 void GrabarSuperBloque(EXT_SIMPLE_SUPERBLOCK *ext_superblock, FILE *fich);
 void GrabarDatos(EXT_DATOS *memdatos, FILE *fich);
+*/
 
 int main()
 {
-	 char *comando[LONGITUD_COMANDO];
-	 char *orden[LONGITUD_COMANDO];
-	 char *argumento1[LONGITUD_COMANDO];
-	 char *argumento2[LONGITUD_COMANDO];
-	 
-	 int i,j;
-	 unsigned long int m;
-     EXT_SIMPLE_SUPERBLOCK ext_superblock;
-     EXT_BYTE_MAPS ext_bytemaps;
-     EXT_BLQ_INODOS ext_blq_inodos;
-     EXT_ENTRADA_DIR directorio[MAX_FICHEROS];
-     EXT_DATOS memdatos[MAX_BLOQUES_DATOS];
-     EXT_DATOS datosfich[MAX_BLOQUES_PARTICION];
-     int entradadir;
-     int grabardatos;
-     FILE *fent;
+   //user input variables (removed the weird structures)
+   char raw[LONGITUD_COMANDO];
+   char *comando; //here we will allocate only the command
+   char *argumento1; //here the first arg
+   char *argumento2; //here the second arg
+   //i see the implementation of 'orden' unnecessary
+   /*
+   unsigned long int m;
+   EXT_SIMPLE_SUPERBLOCK ext_superblock;
+   EXT_BYTE_MAPS ext_bytemaps;
+   EXT_BLQ_INODOS ext_blq_inodos;
+   EXT_ENTRADA_DIR directorio[MAX_FICHEROS];
+   EXT_DATOS memdatos[MAX_BLOQUES_DATOS];
+   EXT_DATOS datosfich[MAX_BLOQUES_PARTICION];
+   int entradadir;
+   int grabardatos;
+   FILE *fent;
+      
+   // Lectura del fichero completo de una sola vez
+   //?...
+      
+   fent = fopen("particion.bin","r+b");
+   fread(&datosfich, SIZE_BLOQUE, MAX_BLOQUES_PARTICION, fent);    
+      
+      
+   memcpy(&ext_superblock,(EXT_SIMPLE_SUPERBLOCK *)&datosfich[0], SIZE_BLOQUE);
+   memcpy(&directorio,(EXT_ENTRADA_DIR *)&datosfich[3], SIZE_BLOQUE);
+   memcpy(&ext_bytemaps,(EXT_BLQ_INODOS *)&datosfich[1], SIZE_BLOQUE);
+   memcpy(&ext_blq_inodos,(EXT_BLQ_INODOS *)&datosfich[2], SIZE_BLOQUE);
+   memcpy(&memdatos,(EXT_DATOS *)&datosfich[4],MAX_BLOQUES_DATOS*SIZE_BLOQUE);
+   */
      
-     // Lectura del fichero completo de una sola vez
-     //?...
-     
-     fent = fopen("particion.bin","r+b");
-     if (fent = NULL) {
-      perror("Error opening patricion file");
-      return 1;
-     }
-     fread(&datosfich, SIZE_BLOQUE, MAX_BLOQUES_PARTICION, fent);    
-     
-     
-     memcpy(&ext_superblock,(EXT_SIMPLE_SUPERBLOCK *)&datosfich[0], SIZE_BLOQUE);
-     memcpy(&directorio,(EXT_ENTRADA_DIR *)&datosfich[3], SIZE_BLOQUE);
-     memcpy(&ext_bytemaps,(EXT_BLQ_INODOS *)&datosfich[1], SIZE_BLOQUE);
-     memcpy(&ext_blq_inodos,(EXT_BLQ_INODOS *)&datosfich[2], SIZE_BLOQUE);
-     memcpy(&memdatos,(EXT_DATOS *)&datosfich[4], MAX_BLOQUES_DATOS*SIZE_BLOQUE);
-     
-     // Bucle de tratamiento de comandos
-     for (;;){
-		 do {
-		 printf (">> ");
-		 fflush(stdin);
-		 fgets(comando, LONGITUD_COMANDO, stdin);
-		 } while (ComprobarComando(comando,orden,argumento1,argumento2) !=0);
-	     if (strcmp(orden,"dir")==0) {
-            Directorio(&directorio,&ext_blq_inodos);
-            continue;
-            }
-         //?...
-         // Escritura de metadatos en comandos rename, remove, copy     
-         Grabarinodosydirectorio(&directorio,&ext_blq_inodos,fent);
-         GrabarByteMaps(&ext_bytemaps,fent);
-         GrabarSuperBloque(&ext_superblock,fent);
-         if (grabardatos)
-           GrabarDatos(&memdatos,fent);
-         grabardatos = 0;
-         //Si el comando es salir se habrán escrito todos los metadatos
-         //faltan los datos y cerrar
-         if (strcmp(orden,"salir")==0){
-            GrabarDatos(&memdatos,fent);
-            fclose(fent);
-            return 0;
+   // main loop
+   for (;;){
+      //do {
+         //this is the user input right here
+         fflush(stdin);
+         printf (">> ");
+         fgets(raw,sizeof(raw),stdin);
+         if(raw[strlen(raw)-1] == '\n'){
+            raw[strlen(raw)-1] = '\0';
          }
-     }
+         //removing the \n
+         
+         //parsing
+         comando = malloc(sizeof(char) * LONGITUD_COMANDO);
+         argumento1 = malloc(sizeof(char) * LONGITUD_COMANDO);
+         argumento2 = malloc(sizeof(char) * LONGITUD_COMANDO);
+
+         char* tk = strtok(raw," ");
+         int v = 0;
+         while(tk != NULL){
+            switch(v){
+               case 0:
+               strcpy(comando,tk);
+               break;
+               case 1:
+               strcpy(argumento1,tk);
+               break;
+               case 2:
+               strcpy(argumento2,tk);
+               break;
+            }v++;
+            tk = strtok(NULL," ");
+         } printf("%s %s %s",comando,argumento1,argumento2);
+      //} while (ComprobarCommando(comando,orden,argumento1,argumento2) != 0);
+            /*if (strcmp(*orden,"dir")==0) {
+                  Directorio(&directorio,&ext_blq_inodos);
+                  continue;
+                  }
+               //?...
+               // Escritura de metadatos en comandos rename, remove, copy     
+               Grabarinodosydirectorio(&directorio,&ext_blq_inodos,fent);
+               GrabarByteMaps(&ext_bytemaps,fent);
+               GrabarSuperBloque(&ext_superblock,fent);
+               if (grabardatos)
+               GrabarDatos(&memdatos,fent);
+               grabardatos = 0;
+
+               //if the command is 'salir' (exit in english) we exit duh
+               //we need the data and we close
+               if (strcmp(orden,"salir")==0){
+                  GrabarDatos(&memdatos,fent);
+                  fclose(fent);
+                  return 0;
+               }*/
+               
+  }
+
+  //we free the user input
+  free(raw);
+  free(comando);
+  free(argumento1);
+  free(argumento2);
 }
+
+/*
+int ComprobarCommando(char *strcomando, char *orden, char *argumento1, char *argumento2)
+{
+   
+   //we read the command and check
+   //incase it doesnt match anything we just return a message
+   //the while will keep asking until its !=0
+   //and we return 0
+   
+
+  int i;
+  char cmds[7][LONGITUD_COMANDO];
+  strcpy(cmds[0],"")
+}
+*/
